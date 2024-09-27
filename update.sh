f0d83fedbf58eea1528d2361bd7c655f55be338c
JGWill <jgi@jgwill.com>
2024-09-27 10:12:48 -0400

    Update

--- update.sh
+++ update.sh
@@ -1,1 +1,6 @@
-(cd _build && mv * ../) && rm -rf _build && git add . && git commit -m "Update" && git push
+
+conda activate prata && \
+	(cd /b/Dropbox/ART/prata/src && \
+	jupyter-book build . --path-output /b/Dropbox/ART/prata-pages) && \
+	echo "Book built" && \
+	(cd _build/html && mv * ../..) && rm -rf _build && git add . && git commit -m "Update" && git push
