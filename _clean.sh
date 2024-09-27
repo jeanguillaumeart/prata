5c67a746330825d4a78a420f8cc38c56bd6cff7c
JGWill <jgi@jgwill.com>
2024-09-27 10:47:02 -0400

    scripts

---
+++ _clean.sh
@@ -0,0 +1,15 @@
+
+for f in genindex.html \
+_images \
+index.html \
+intro.html \
+markdown.html \
+markdown-notebooks.html \
+notebooks.html \
+objects.inv \
+search.html \
+searchindex.js \
+_sources \
+_sphinx_design_static \
+_static ;do rm -rf $f; done
+
